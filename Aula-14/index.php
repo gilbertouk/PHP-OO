bd78e8cf5ea631c102f59e2a230189a26f8b1d87
Gilberto Antonio <103536077+gilbertouk@users.noreply.github.com>
2022-09-01 23:55:38 +0100

    Atualização

--- Aula-14/index.php
+++ Aula-14/index.php
@@ -10,12 +10,15 @@
     <pre>
     <?php 
         require_once "Video.php";
+        require_once "Pessoa.php";
 
         $v[0] = new Video("Aula 1 de POO");
         $v[1] = new Video("Aula 12 de PHP");
         $v[2] = new Video("Aula 15 de HTML5");
 
+        $p = new Pessoa("Gilberto", 22, "M");
         print_r($v);
+        print_r($p);
     ?>
     </pre>
 </body>
